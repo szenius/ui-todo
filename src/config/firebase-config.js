2eba5691df7d0cd358cc601772d3a40601da1949
Jian Sheng <jiansheng.govtech@outlook.com>
2020-09-09 19:04:45 +0800

    Add app config to read env file

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,17 +1,8 @@
 import firebase from "firebase";
 // Required for side-effects
 import "firebase/firestore";
+import { config } from "./app-config";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBF6LS7jEjzm9HTzLpzG9_NZhQJ1WQ_E3A",
-  authDomain: "regal-moment-265311.firebaseapp.com",
-  databaseURL: "https://regal-moment-265311.firebaseio.com",
-  projectId: "regal-moment-265311",
-  storageBucket: "regal-moment-265311.appspot.com",
-  messagingSenderId: "727774404761",
-  appId: "1:727774404761:web:ba274fa82e3cb8933a9808"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config.firebase);
 
 export const fireStore = firebase.firestore();
